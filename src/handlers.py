05fcef1e9e9f4dcef3306c874749861809f9c2ed
Chrisnisch <mihail.gusev.95@yandex.ru>
2023-03-09 16:12:32 +0300

    some changes

--- src/handlers.py
+++ src/handlers.py
@@ -1,4 +1,4 @@
-# добавить пиздюли за пустые команды set и edit, форматирование текста, решить проблему чат-террористов
+# возможно стоит перенести тексты сообщений бота в отдельный файл
 from aiogram import types, Dispatcher
 import re
 import datetime
@@ -6,6 +6,7 @@ import datetime
 cur_day = ""
 cur_time = ""
 name = ""
+white_list = set(open("white-list.txt", "r", encoding="UTF-8").read().split())
 
 
 def command_help(message: types.Message):
@@ -20,21 +21,34 @@ def command_lunch(message: types.Message):
     global cur_day, name, cur_time
     f = open("lunch.txt", "r")
     cur_lunch = f.read()
+    if cur_lunch == "":
+        return message.reply("Нет информации")
     cur_lunch = cur_lunch[0].upper() + cur_lunch[1:]
     f.close()
-    return message.reply(f'Сегодня {cur_day} на обед:\n{cur_lunch}'
-                         f'\nИнформация предоставлена @{name} в {cur_time}'
-                         f'\nЕсли что-то поменялось, напишите /edit_lunch и новый состав обеда')
+    return message.reply(f'Сегодня <b>{cur_day}</b> на обед:\n<b>{cur_lunch}</b>'
+                         f'\nИнформация предоставлена @{name} в {cur_time}', parse_mode="HTML")
 
 
 def command_set_lunch(message: types.Message):
-    # установить обэд
+    # установить и редактировать обэд
     global cur_day, name, cur_time
     cur_day = datetime.date.today().strftime('%d.%m')
     cur_time = datetime.datetime.now().time().strftime('%H:%M')
     name = message.from_user.username
+    s = re.search("""([А-я\s,.])+""", message.text)
+    if s is not None:
+        s = s.group()[1:]
+        t = re.split("""[\s,.-]+""", s)
+        print(t)
+        for word in t:
+            if word not in white_list:
+                return message.reply("Я тебе не верю.")
+            else:
+                continue
+    else:
+        return message.reply("Пустое сообщение")
     f = open("lunch.txt", "w")
-    f.write(re.search("""([А-я\s,.])+""", message.text).group()[1:])
+    f.write(s)
     f.close()
     return message.reply("Состав обеда на сегодня добавлен")
 
@@ -45,8 +59,20 @@ def command_edit_lunch(message: types.Message):
     cur_day = datetime.date.today().strftime('%d.%m')
     cur_time = datetime.datetime.now().time().strftime('%H:%M')
     name = message.from_user.username
+    s = re.search("""([А-я\s,.])+""", message.text)
+    if s is not None:
+        s = s.group()[1:]
+        t = re.split("""[\s,.-]+""", s)
+        print(t)
+        for word in t:
+            if word not in white_list:
+                return message.reply("Я тебе не верю.")
+            else:
+                continue
+    else:
+        return message.reply("Пустое сообщение")
     f = open("lunch.txt", "w")
-    f.write(re.search("""([А-я\s,.])+""", message.text).group()[1:])
+    f.write(s)
     f.close()
     return message.reply("Состав обеда обновлён")
 
